7499bd07fc2fabda9b744cdd66e9c0fc92159396
choonpal <129235749+choonpal@users.noreply.github.com>
2025-11-15 13:36:23 +0900

    Implement price calculation for ramen orders
    Added price calculation for ramen orders based on toppings and sides.

--- src/ramen_table/ramen_table/order_node.py
+++ src/ramen_table/ramen_table/order_node.py
@@ -4,6 +4,8 @@
 import rclpy
 from rclpy.node import Node
 from ramen_interfaces.msg import RamenOrder
+topping_dir={"x":0,"치즈":500,"떡":500,"만두":1000,"계란":500,"파":300,"김치":1000,"콩나물":500,"햄":1000,"고추":300}
+side_dir={"음료":2000,"김밥":3000,"튀김":2000,"떡볶이":4000,"순대":3000,"어묵":3000}
 
 class OrderNode(Node):
     def __init__(self):
@@ -11,7 +13,7 @@ class OrderNode(Node):
         self.publisher_ = self.create_publisher(RamenOrder, 'new_order', 10)
         self.get_logger().info('--- 라면 주문 시스템 (Table) ---')
         self.send_test_order()
-
+    
     def send_test_order(self):
         # TODO: 실제 UI나 터미널 입력을 통해 이 데이터를 받아야 합니다.
         # 여기서는 하드코딩된 테스트 주문을 사용합니다.
@@ -38,7 +40,13 @@ class OrderNode(Node):
         self.get_logger().info(f' - 토핑: {", ".join(msg.toppings)}')
         self.get_logger().info(f' - 사이드: {", ".join(msg.sides)}')
         self.get_logger().info('---------------------------------')
-
+        msg.price = self.calculate_price(toppings_list, sides_list)
+        self.get_logger().info(f' - 총 가격: {msg.price}원')
+    def calculate_price(self,toppings_list, sides_list):
+        base_price = 3000  # 기본 라면 가격
+        topping_price = sum(topping_dir[t] for t in toppings_list if t in topping_dir) # 토핑당 가격
+        side_price = sum(side_dir[s] for s in sides_list if s in side_dir)  # 사이드당 가격
+        return base_price + topping_price + side_price
 
 def main(args=None):
     rclpy.init(args=args)
